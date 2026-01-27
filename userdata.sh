e7bb63b66c87a0301e984e3854abcef2cd224400
Mikel Emaldi <m.emaldi@deusto.es>
2026-01-27 08:51:46 +0100

    Indicando de manera explícita el directorio de trabajo de los diferentes comandos

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dnf install -y java git
 wget https://dlcdn.apache.org/nifi/2.7.2/nifi-2.7.2-bin.zip
-unzip nifi-2.7.2-bin.zip
-git clone https://github.com/memaldi/muba_practica_nifi
+unzip nifi-2.7.2-bin.zip -d /home/ec2-user
+git clone https://github.com/memaldi/muba_practica_nifi /home/ec2-user/muba_practica_nifi
 cp /home/ec2-user/muba_practica_nifi/nifi.properties /home/ec2-user/nifi-2.7.2/conf/
 ./home/ec2-user/nifi-2.7.2/bin/nifi.sh start
