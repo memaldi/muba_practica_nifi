6062197b61d37a715771b69bfd864efc6ab1a841
Mikel Emaldi <m.emaldi@deusto.es>
2026-01-27 12:03:02 +0100

    Intentando solucionar el problema del truststore

--- userdata.sh
+++ userdata.sh
@@ -4,5 +4,5 @@ wget https://dlcdn.apache.org/nifi/2.7.2/nifi-2.7.2-bin.zip
 unzip nifi-2.7.2-bin.zip -d /home/ec2-user
 git clone https://github.com/memaldi/muba_practica_nifi /home/ec2-user/muba_practica_nifi
 cp /home/ec2-user/muba_practica_nifi/nifi.properties /home/ec2-user/nifi-2.7.2/conf/
-chown -R ec2-user:ec2-user /home/ec2-user/nifi-2.7.2/
+#chown -R ec2-user:ec2-user /home/ec2-user/nifi-2.7.2/
 ./home/ec2-user/nifi-2.7.2/bin/nifi.sh start
