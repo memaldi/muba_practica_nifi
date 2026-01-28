01e021d9274dea21923cf2e3b0cafe4170c84813
Mikel Emaldi <m.emaldi@deusto.es>
2026-01-28 08:27:49 +0100

    Creando los buckets S3 necesarios automáticamente

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chown ec2-user:ec2-user /home/ec2-user/.aws
 dnf install -y java git
 wget https://dlcdn.apache.org/nifi/2.7.2/nifi-2.7.2-bin.zip
 unzip nifi-2.7.2-bin.zip -d /home/ec2-user
@@ -6,3 +7,11 @@ git clone https://github.com/memaldi/muba_practica_nifi /home/ec2-user/muba_prac
 cp /home/ec2-user/muba_practica_nifi/nifi.properties /home/ec2-user/nifi-2.7.2/conf/
 #chown -R ec2-user:ec2-user /home/ec2-user/nifi-2.7.2/
 ./home/ec2-user/nifi-2.7.2/bin/nifi.sh start
+
+export HEART_DISEASE_BUCKET=heart-disease-muba-$(uuidgen)
+export HIGH_CHOLESTEROL_BUCKET=high-cholesterol-muba-$(uuidgen)
+export NORMAL_CHOLESTEROL_BUCKET=normal-cholesterol-muba-$(uuidgen)
+
+aws s3api create-bucket --bucket $HEART_DISEASE_BUCKET
+aws s3api create-bucket --bucket $HIGH_CHOLESTEROL_BUCKET
+aws s3api create-bucket --bucket $NORMAL_CHOLESTEROL_BUCKET
