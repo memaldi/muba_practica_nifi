7a1e99145bf59a150624afe6a6bc344b832faf5c
Mikel Emaldi <m.emaldi@deusto.es>
2026-03-12 10:39:10 +0100

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dnf install -y java git
-wget https://dlcdn.apache.org/nifi/2.7.2/nifi-2.7.2-bin.zip
+wget https://dlcdn.apache.org/nifi/2.8.0/nifi-2.8.0-bin.zip
 unzip nifi-2.7.2-bin.zip -d /home/ec2-user
 git clone https://github.com/memaldi/muba_practica_nifi /home/ec2-user/muba_practica_nifi
 cp /home/ec2-user/muba_practica_nifi/nifi.properties /home/ec2-user/nifi-2.7.2/conf/
