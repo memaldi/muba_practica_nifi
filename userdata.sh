9623b49209f0f42bc20a820dc38c85f758663319
Mikel Emaldi <m.emaldi@deusto.es>
2026-03-12 11:26:09 +0100

    Update download and unzip commands for NiFi

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dnf install -y java git
-wget --no-verbose --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 https://dlcdn.apache.org/nifi/2.8.0/nifi-2.8.0-bin.zip
-unzip nifi-2.7.2-bin.zip -d /home/ec2-user
+wget --limit-rate=500k --no-verbose --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 https://dlcdn.apache.org/nifi/2.8.0/nifi-2.8.0-bin.zip
+unzip nifi-2.8.0-bin.zip -d /home/ec2-user
 git clone https://github.com/memaldi/muba_practica_nifi /home/ec2-user/muba_practica_nifi
 cp /home/ec2-user/muba_practica_nifi/nifi.properties /home/ec2-user/nifi-2.7.2/conf/
 #chown -R ec2-user:ec2-user /home/ec2-user/nifi-2.7.2/
-./home/ec2-user/nifi-2.7.2/bin/nifi.sh start
+/home/ec2-user/nifi-2.7.2/bin/nifi.sh start
 
 export HEART_DISEASE_BUCKET=heart-disease-muba-$(uuidgen)
 export HIGH_CHOLESTEROL_BUCKET=high-cholesterol-muba-$(uuidgen)
